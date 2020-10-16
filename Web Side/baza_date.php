1dd740f5b4d466351992c65d741db2d8a19bc4ad
Andrei Biro <andreibiro2003@gmail.com>
2020-10-16 14:40:27 +0300

    php connection to mysql not working

--- Web Side/baza_date.php
+++ Web Side/baza_date.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername="192.168.0.109";
-$username="pi";
-$password="pi";
+$username="root";
+$password="Parola1";
 $dbname="baza_date_restaurant";
 
 
@@ -12,6 +12,18 @@ if (!$conn){
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$
+$nume=$_POST['nume_restaurant'];
+$live_count=$_POST['live_count'];
+$rating=$_POST['rating'];
 
+
+$sql="INSERT INTO restaurant (nume,live_count,rating)
+Values($nume,$live_count,$rating)";
+
+if(mysqli_querry($conn,$sql)){
+    echo "New record created succesfuly!";
+}else{
+    echo "Error: " .$sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
 ?>
